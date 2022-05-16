0ba406a7c27a1fe0294bcd6f9d5d693c1eb6134f
mertcakmak2 <mertcakmak2@gmail.com>
2022-05-16 21:48:49 +0300

    kafka unit test

---
+++ src/main/java/com/test/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,28 @@
+package com.test.kafka.consumer;
+
+import com.test.kafka.model.Todo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "todo-topic",
+            concurrency = "${spring.kafka.consumer.level.concurrency:3}",
+            properties = {"spring.json.value.default.type=com.test.kafka.model.Todo"})
+    public void todoTopicListener(Todo todo) {
+        logger.info("Received a todo message {}", todo.getTodo());
+    }
+
+    @KafkaListener(topics = "string-message-topic",
+            concurrency = "${spring.kafka.consumer.level.concurrency:3}",
+            properties = {"spring.json.value.default.type=java.lang.String"})
+    public void stringTopicListener(String message) {
+        logger.info("Received a string message {}", message);
+    }
+
+}
